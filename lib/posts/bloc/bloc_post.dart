c34f09521f94ae1a116d9a4172ffecef1af975bd
Darshfb <88770750+Darshfb@users.noreply.github.com>
2022-05-19 17:51:21 +0200

    first commit

--- lib/posts/bloc/bloc_post.dart
+++ lib/posts/bloc/bloc_post.dart
@@ -8,9 +8,8 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:http/http.dart' as http;
 import 'package:stream_transform/stream_transform.dart';
 
-
 const throttleDuration = Duration(milliseconds: 100);
-const _postLimit = 20;
+const _postLimit = 10;
 
 EventTransformer<E> throttleDroppable<E>(Duration duration) {
   return (events, mapper) {
@@ -19,30 +18,13 @@ EventTransformer<E> throttleDroppable<E>(Duration duration) {
 }
 
 class PostBloc extends Bloc<PostEvent, PostState> {
-  PostBloc({required this.httpClient}) : super(PostState()) {
-    on<PostFetched>(_onPostFetched,
-      transformer: throttleDroppable(throttleDuration),
-    );
-  }
-
   final http.Client httpClient;
 
-  FutureOr<void> _onPostFetched(PostFetched event,
-      Emitter<PostState> emit) async {
-    if (state.hasReachedMax) return;
-    try {
-      if (state.status == PostStatus.initial) {
-        final posts = await _fetchPosts();
-        return emit(state.copyWith(
-            status: PostStatus.success, posts: posts, hasReachedMax: false));
-      }
-      final posts = await _fetchPosts(state.posts.length);
-      emit(posts.isEmpty ? state.copyWith(hasReachedMax: true) : state.copyWith(
-          status: PostStatus.success, posts: List.of(state.posts)
-        ..addAll(posts), hasReachedMax: false));
-    } catch (_) {
-      emit(state.copyWith(status: PostStatus.failure));
-    }
+  PostBloc({required this.httpClient}) : super(PostState()) {
+    on<PostFetched>(
+      _onPostFetched,
+      transformer: throttleDroppable(throttleDuration),
+    );
   }
 
   Future<List<Post>> _fetchPosts([int startIndex = 0]) async {
@@ -66,6 +48,24 @@ class PostBloc extends Bloc<PostEvent, PostState> {
     throw Exception('error fetching posts');
   }
 
-
-
+  Future<void> _onPostFetched(
+      PostFetched event, Emitter<PostState> emit) async {
+    if (state.hasReachedMax) return;
+    try {
+      if (state.status == PostStatus.initial) {
+        final posts = await _fetchPosts();
+        return emit(state.copyWith(
+            status: PostStatus.success, posts: posts, hasReachedMax: false));
+      }
+      final posts = await _fetchPosts(state.posts.length);
+      emit(posts.isEmpty
+          ? state.copyWith(hasReachedMax: true)
+          : state.copyWith(
+              status: PostStatus.success,
+              posts: List.of(state.posts)..addAll(posts),
+              hasReachedMax: false));
+    } catch (_) {
+      emit(state.copyWith(status: PostStatus.failure));
+    }
+  }
 }
